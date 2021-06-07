84005c1a2d3622907dbfdbbb6cb1e066c07d9634
1611lush <1611lush@gmail.com>
2021-06-08 01:04:07 +0400

    Add js

--- main.js
+++ main.js
@@ -2,6 +2,10 @@
 
   let body = document.querySelector("body");
   let outer = document.querySelector('.outer');
+  let input = document.querySelectorAll("input");
+
+  const soundDown = new Audio('pop.mp3');
+  const soundUp = new Audio('pop2.mp3');
 
 
   function ready() {
@@ -9,14 +13,12 @@
     function createInner(color){
       let e = document.createElement('div');
       e.className = color;
-
       for (let i = 0; i < 6; i++) {
           let inner = document.createElement('div');
-          let checkbox = '<input id='+color+i+' type="checkbox" name='+color+i+'><label for='+color+i+'></label>';
+          let checkbox = '<input id='+color+i+' type="checkbox" name='+color+i+' checked><label for='+color+i+'></label>';
           inner.innerHTML = checkbox;
           e.append(inner);
       }
-
       outer.appendChild(e);
     }
 
@@ -27,9 +29,26 @@
     createInner('blue');
     createInner('purple');
   }
-
   document.addEventListener("DOMContentLoaded", ready);
 
+
+
+  function onPopClick(e) {
+    let target = e.target;
+     if (target.tagName === "LABEL") {  
+      let checkbox = target.parentElement.querySelector('input');
+      let isChecked = checkbox.checked;
+      if (!isChecked) {
+        target.classList.add('checked');
+        soundUp.play();
+      } else {
+        target.classList.remove('checked');
+        soundDown.play();
+      }
+    }   
+  }
+  body.addEventListener('mousedown', onPopClick);
+  body.addEventListener('touchstart', onPopClick);
 
 
 
